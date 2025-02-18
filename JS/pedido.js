807fc5949507a97eb1876cca63b406d3a1526d4a
Matheus Borborema <matheusea8@gmail.com>
2025-02-18 14:57:59 -0300

    Add files via upload

---
+++ JS/pedido.js
@@ -0,0 +1,11 @@
+function irCardapio(){
+    window.location.href = "./cardapio.html"
+}
+
+function irSugestão(){
+    window.location.href = "./sugestoes.html"
+  }
+
+  function voltarHome() {
+    window.location.href = "./index.html"
+  }
---
+++ JS/sugestoes.js
@@ -0,0 +1,26 @@
+function btn(){
+    var cxText = document.getElementById("cxText");
+    cxText.value = ''
+}
+
+function voltarHome(){
+    window.location.href = "./index.html"
+}
+function btnSacola(){
+    window.location.href = "./carrinho.html"
+  }
+  
+  function btnCardapio(){
+      window.location.href = "./pedido.html"
+    }
+  
+    function irSugestão(){
+      window.location.href = "./sugestoes.html"
+    }
+    function btnPerfil(){
+      window.location.href = "./userpage.html"
+    }
+    
+    function btnPerfilAdm(){
+      window.location.href = "./admpage.html"
+    }
